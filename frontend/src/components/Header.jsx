6f459b83b69555ac07a2d6c316b9deeb963b9d96
Mansii73 <mansisiiingh012@gmail.com>
2025-04-27 19:18:42 +0530

    update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
 export default function Header() {
   return (
     <>
       <div className="py-4 mx-[10px] shadow-xl bg-white w-full h-full object-cover">
         <div className="flex flex-wrap justify-center items-center mt-4 space-x-6 w-full">
-          <img 
-            src="https://www.clayblossoms.com/uploads/2/3/9/1/23910383/new-clayblossoms-logo22-web.png" 
-            className="w-[140px] h-auto transition-transform duration-300 hover:translate-x-2 cursor-pointer" 
+          <img
+            src="https://www.clayblossoms.com/uploads/2/3/9/1/23910383/new-clayblossoms-logo22-web.png"
+            className="w-[140px] h-auto transition-transform duration-300 hover:translate-x-2 cursor-pointer"
             alt="Clayblossoms Logo"
           />
           <form className="w-[45%] max-w-md">
-            <input 
-              type="text" 
-              placeholder="Type for search" 
-              className="border-2 border-gray-300 rounded-md p-2 w-full focus:outline-none focus:border-blue-400" 
+            <input
+              type="text"
+              placeholder="Type for search"
+              className="border-2 border-gray-300 rounded-md p-2 w-full focus:outline-none focus:border-blue-400"
             />
           </form>
           <ul className="flex space-x-8 text-gray-700 font-semibold">
@@ -26,7 +26,6 @@ export default function Header() {
           </ul>
         </div>
       </div>
-
     </>
   );
 }
