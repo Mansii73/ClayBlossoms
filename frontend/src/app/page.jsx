7118e8861306416c520ded3ddc137df2ef6e7746
meharfatima83 <meharfatima0522@gmail.com>
2025-04-28 21:37:04 +0530

    changes done

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,17 +1,37 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
-import App from '../components/App'
-import Card from '../components/Card'
+import App from '../components/App';
+import App from '../components/Home';
+import App from '../components/About';
+import App from '../components/Products';
+import App from '../components/Contact';
+import App from '../components/Signup';
+import App from '../components/SingleProduct/:id';
+import App from '../components/Cart';
+import App from '../components/Error';
+import App from '../components/GlobalStyle';
+import App from '../components/Styled-components';
+import App from '../components/ThemeProvider';
+
 
 
 const Home = () => {
   return (
+    <themeProvider theme={theme}>
     <div>
+   
       <div><Navbar/></div>
+      <div><Home/></div>
+      <div><About/></div>
+      <div><Products/></div>
       <div><App/></div>
-      <div><Card/></div>
+      <div><Contact/></div>
+      <div><SingleProduct/></div>
+      <div><Cart/></div>
+      <div><Error/></div>
       
     </div>
+    </themeProvider>
   )
 }
 
